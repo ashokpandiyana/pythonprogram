9028e28052565355872510c732f08af4dbb0c762
ashokpandiyana <32787076+ashokpandiyana@users.noreply.github.com>
2018-03-18 14:41:56 +0530

    Update indexarray.py

--- beginner level/indexarray.py
+++ beginner level/indexarray.py
@@ -3,4 +3,4 @@ no = int(input("insert how many elements you want:"))
 for i in range(0,no):
     array.append(int(input("Enter next no :")))
 for i in range(len(array)):
-   print ( i, array[i])
+   print ( array[i],i )
