ea66f3c1d992356735e7e46bad906e8c370706d2
ashokpandiyana <32787076+ashokpandiyana@users.noreply.github.com>
2018-03-20 15:03:07 +0530

    Update factorial3.py

--- beginner level/factorial3.py
+++ beginner level/factorial3.py
@@ -4,7 +4,7 @@ if (num<0):
    print("no factorial")
 elif num == 0:
    print("1")
-elif (num<=20):
+elif (num<=25):
    for i in range(1,num + 1):
        factorial = factorial*i
 print("The factorial of is ",factorial)
