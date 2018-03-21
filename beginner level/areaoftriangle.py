4fa0e7a465e4424b68e36d44f564880c580f7352
ashokpandiyana <32787076+ashokpandiyana@users.noreply.github.com>
2018-03-21 10:37:31 +0530

    Create areaoftriangle.py

---
+++ beginner level/areaoftriangle.py
@@ -0,0 +1,7 @@
+height=int(input("enter the height"))
+breadth=int(input("enter the breadth"))
+if (height >=1 and height<=100000):
+  if (breadth >=1 and breadth<=100000):
+    area=height*breadth
+    area=area/2
+    print(area)
